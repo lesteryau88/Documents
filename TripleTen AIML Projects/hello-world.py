86cd2fe30a19f406f1b957cac558471b481ed58f
LesterYau <lester.yau88@gmail.com>
2026-01-13 23:38:58 -0800

    Add random greeting generator function

--- TripleTen AIML Projects/hello-world.py
+++ TripleTen AIML Projects/hello-world.py
@@ -1,2 +1,17 @@
-print("Hello, world!")
-print("Learning about commits today!")
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+
+print(get_random_greeting())
+print("Learning about branches today!")
